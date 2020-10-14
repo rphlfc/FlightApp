863331f0cebce3b906d6a4a151aa76ca6383cbb2
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-10-14 11:05:00 -0300

    Initial Commit

---
+++ FlightApp/FlightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAppApp.swift
+//  FlightApp
+//
+//  Created by Raphael Cerqueira on 14/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FlightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
